d1fe49d56ac95af1944d6b2592d1016e96599b3a
Joshua W <jaxxa90@gmail.com>
2018-05-08 07:34:42 +1000

    Adding Blank Settings and Patch Framework

---
+++ Source/ED-Embrasures/Patcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Harmony;
+
+namespace EnhancedDevelopment.Embrasures
+{
+    [StaticConstructorOnStartup]
+    internal class Patcher
+    {
+        static Patcher()
+        {
+            string _LogLocation = "EnhancedDevelopment.Excalibur.Patches.Patcher(): ";
+
+            Log.Message(_LogLocation + "Starting.");
+
+            //Create List of Patches
+            List<Patch> _Patches = new List<Patch>();
+            ////_Patches.Add(new Patches.PatchProjectile());
+
+            //Create Harmony Instance
+            HarmonyInstance _Harmony = HarmonyInstance.Create("EnhancedDevelopment.Embrasures");
+
+            //Iterate Patches
+            _Patches.ForEach(p => p.ApplyPatchIfRequired(_Harmony));
+
+            Log.Message(_LogLocation + "Complete.");
+        }
+
+        /// <summary>
+        /// Debug Logging Helper
+        /// </summary>
+        /// <param name="objectToTest"></param>
+        /// <param name="name"></param>
+        /// <param name="logSucess"></param>
+        public static void LogNULL(object objectToTest, String name, bool logSucess = false)
+        {
+            if (objectToTest == null)
+            {
+                Log.Error(name + " Is NULL.");
+            }
+            else if (logSucess)
+            {
+                Log.Message(name + " Is Not NULL.");
+            }
+        }
+
+    }
+
+}
---
+++ Source/ED-Embrasures/Settings/ModSettings_Embrasures.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Embrasures.Settings
+{
+    class ModSettings_Embrasures : ModSettings
+    {
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+        }
+
+
+
+        public void DoSettingsWindowContents(Rect canvas)
+        {
+
+        }
+
+    }
+
+}
