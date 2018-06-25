d9a490587895c1a9865e1f075a7ecc1084533974
Joshua W <jaxxa90@gmail.com>
2018-06-26 08:08:26 +1000

    Correcting issue with setting the fill amount.

--- Source/ED-Embrasures/Patch_EmbrasureDef.cs
+++ Source/ED-Embrasures/Patch_EmbrasureDef.cs
@@ -14,8 +14,9 @@ namespace EnhancedDevelopment.Embrasures
         protected override void ApplyPatch(HarmonyInstance harmony = null)
         {
             ThingDef _EmbrasureDef = ThingDef.Named("ED_Embrasure");
+            
+            _EmbrasureDef.fillPercent = (float)(Mod_Embrasures.Settings.FillPercent)/100f;
 
-            _EmbrasureDef.fillPercent = (float)(Mod_Embrasures.Settings.FillPercent/100);
             _EmbrasureDef.costStuffCount = Mod_Embrasures.Settings.StuffCost;
         }
 
